9bf7bb3c20ccc7a9ad22bc376bdfbdad850dcdcf
juagiag1 <44643456+juagiag1@users.noreply.github.com>
2019-12-29 19:17:53 +0100

    estado inicial

---
+++ create.php
@@ -0,0 +1,17 @@
+<?php
+  include('./librerias/conexion.php');
+
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $name = $_POST['name'];
+
+  // Insertamos los valores en la tabla de datos
+  $sql = "";
+  $result = mysqli_query($link, $slq);
+
+  if($result){
+    header("Location:index.php?createuser=ok");
+  }else{
+    header("Location:index.php?createuser=ko");
+  }
+?>
---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+  include('./librerias/conexion.php');
+
+  // Borramos los valores en la tabla de datos
+  $sql = "";
+  $result = mysqli_query($link, $slq);
+
+  if($result){
+    header("Location:index.php?deleteuser=ok");
+  }else{
+    header("Location:index.php?deleteuser=ko");
+  }
+?>
